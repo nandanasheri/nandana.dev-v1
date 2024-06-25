4667f1770b9f03000b6d11ab3d1470963d771d79
Nandana <nandanasheri@gmail.com>
2024-06-25 11:35:06 -0500

    fixed warning + visible footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,7 @@ import { FaLinkedin } from "react-icons/fa";
 
 export default function Footer() {
     return (
-        <div className="bg-reen mt-20 mb-8 flex items-center justify-center gap-5">
+        <div className="bg-main-bg mt-20 mb-8 p-4 flex items-center justify-center gap-5">
             <IconContext.Provider value={{ className: "shared-class", size: 35 }}>
             <h3 className="text-grey">Built and designed by Nandana Sheri &lt;3</h3>
             <a href="https://github.com/nandanasheri/nandana.dev-v1">
